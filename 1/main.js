76ad2fd8bf4790a19bcd428c29cb6bb5a09198df
deer000 <camel000k@gmail.com>
2020-04-04 21:27:38 +0900

    200404

---
+++ 1/main.js
@@ -0,0 +1,26 @@
+    function minus(a, b){
+    return a+b;
+    }
+
+    var car = minus(4, 2);
+    console.log(car);
+
+   // ---------------------------------//
+
+    function gift(b, c){
+     return b*c;
+    }
+
+    var bus = gift(3, 5)
+    console.log(bus);
+
+    function run(d, e){
+        return d/e;
+    }
+
+    var get = run(7,3);
+    console.log(get);
+
+
+    //----------------------------//
+
