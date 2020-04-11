7fa90ffc659c229761701905f84deb78622b645e
deer000 <camel000k@gmail.com>
2020-04-11 19:54:10 +0900

    20200411_2

--- 3/main.js
+++ 3/main.js
@@ -12,3 +12,17 @@ var e = 10;
 var sum = (a+b-c)*d/e;
 document.write(sum);
 document.write('<br/>');
+
+var str1 = "자바스크립트";
+var str2 = "기초";
+var sum2 = str1 + str2;
+document.write(sum);
+document.write('<br/>');
+
+var time1 = "현재 시간은";
+var time2 = 1;
+var time3 = 2;
+var str3 = "시 입니다.."
+var str4 = time1+time2+time3+str3
+document.write(str4);
+document.write('<br/>');
