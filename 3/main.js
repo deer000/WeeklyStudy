d9fa88929fd67090ebb3cd80f8737452a21ec6a6
deer000 <camel000k@gmail.com>
2020-04-11 19:40:58 +0900

    20200411_1

---
+++ 3/main.js
@@ -0,0 +1,14 @@
+var six = 6;
+var seven = 7;
+
+document.write(six +seven);
+document.write('<br/>');
+
+var a = 40;
+var b = 30;
+var c = 20;
+var d = 5;
+var e = 10;
+var sum = (a+b-c)*d/e;
+document.write(sum);
+document.write('<br/>');
