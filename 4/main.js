a2020521196ae66587ffac8a51d4c69436e0c4d7
deer000 <camel000k@gmail.com>
2020-04-12 14:45:36 +0900

    20200412_7

--- 4/main.js
+++ 4/main.js
@@ -36,7 +36,27 @@ if(a != b){
 var a = 50;
 var b = 20;
 
+if(a>b) {
+    document.write('a가 b보다 크다');
+    } else {
+    document.write('a가 b보다 작다');
+    }
+}
+
+// 조건문의 축약
+var a = 50;
+var b = 20;
+
 if(a>b)
+    document.write('a가 b보다 크다');
+else
+    document.write('a가 b보다 작다');
+
+// 조건문의 축약
+var a = 50;
+var b = 20;
+
+if(a>b) ?
     document.write('a가 b보다 크다');
 else
     document.write('a가 b보다 작다');
