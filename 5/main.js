1b4e8cf9b57ec1d248e9a97e1404af9405d44968
deer000 <camel000k@gmail.com>
2020-04-12 15:00:01 +0900

    20200412_11

---
+++ 5/main.js
@@ -0,0 +1,8 @@
+// 조건문의 축약
+var a = 50;
+var b = 20;
+
+if(a>b)
+    document.write('a가 b보다 크다');
+else
+    document.write('a가 b보다 작다');
---
+++ 6/main.js
@@ -0,0 +1,8 @@
+// 조건문의 축약
+var a = 50;
+var b = 20;
+
+(a>b) ?
+    document.write('a가 b보다 크다'):
+
+    document.write('a가 b보다 작다');
