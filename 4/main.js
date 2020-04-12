46db04157f3817aff8bea781a9922d654400822d
deer000 <camel000k@gmail.com>
2020-04-12 13:54:13 +0900

    20200412_1

---
+++ 4/main.js
@@ -0,0 +1,21 @@
+var a = 18;
+var b = 44;
+var c = 10;
+
+if(a > b){
+    document.write('a가 b보다 크다');
+} else if(b > c){
+    document.write('b가 c보다 크다');
+} else if(b < c){
+    document.write('b가 c보다 작다');
+} else {
+    document.write('어쩌라구');
+}
+// 논리 연산자
+var a = 10;
+var b = 22;
+var c = 48;
+
+if(a<c && b<c){
+    document.write('a가 c보다 작고 b가 c보다 작다');
+}
