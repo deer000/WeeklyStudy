ac7f63e75244d8293b00e869994f51cf88e10e65
deer000 <camel000k@gmail.com>
2020-04-12 14:22:42 +0900

    20200412_4

--- 4/main.js
+++ 4/main.js
@@ -19,3 +19,25 @@ var c = 48;
 if(a<c || b>c){
     document.write('a가 c보다 작고 b가 c보다 작다');
 }
+
+// 조건문의 응용
+var a = 90;
+var b = 22;
+
+if(a != b){
+    if(a>b){
+        document.write('a가 b보다 크다');
+    } else{
+        document.write('a가 b보다 작다');
+    }
+}
+
+// 조건문의 축약
+var a = 50;
+var b = 20;
+
+if(a>b){
+    document.write('a가 b보다 크다');
+} else {
+    document.write('a가 b보다 작다');
+}
