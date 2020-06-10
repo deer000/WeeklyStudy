9545506c199b32b663498a85e33fdb39f1af4ac3
deer000 <camel000k@gmail.com>
2020-06-10 19:23:35 +0900

    11

---
+++ 11/main.js
@@ -0,0 +1,12 @@
+var i = 6;
+document.write(i + '<br/>');
+i = i + 6;
+document.write(i + '<br/>');
+i = i + 6;
+document.write(i + '<br/>');
+i = i + 6;
+document.write(i + '<br/>');
+i = i + 6;
+document.write(i + '<br/>');
+i = i + 6;
+document.write(i + '<br/>');
